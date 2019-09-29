8ebf2b179e4b4c926f8bd64667f49c5d8ef94556
Amber Jones <amberlovescats14@gmail.com>
2019-09-29 13:36:48 -0500

    added table

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,36 @@
+let jobs = []
+
+  const getAllJobs = () => {
+    fetch('/jobs')
+    .then(res => res.json())
+    .then(data => jobs = [...data])
+  }
+  // getAllJobs()
+
+const postOptions = {
+  method: 'POST',
+  header: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify()
+}
+
+const addJob = (district = district.value, school = school.value, grade = grade.value, subject = subject.value ) => {
+  if(!district || !school || !grade || !subject) (
+    alert(`All fields must be complete`)
+  )
+  fetch('/jobs', postOptions)
+    .then(res => res.json)
+    .then(data => jobs = [...data])
+}
+
+const editJobOptions = {
+  
+}
+const editJob = () => {
+
+}
+  
 // Show that dashboard.js loaded
 console.log("Dashboard.js is loaded");
 
